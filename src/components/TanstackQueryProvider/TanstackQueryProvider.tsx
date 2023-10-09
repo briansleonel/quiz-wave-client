0a7eb58a1e39f2848b79dc9e97031399ce877a9f
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-10-09 10:36:05 -0300

    feat: add query provider

---
+++ src/components/TanstackQueryProvider/TanstackQueryProvider.tsx
@@ -0,0 +1,22 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useState } from "react";
+
+export function TanstackQueryProvider({ children }: React.PropsWithChildren) {
+    const [queryClient] = useState(
+        new QueryClient({
+            defaultOptions: {
+                queries: {
+                    refetchOnWindowFocus: false,
+                },
+            },
+        })
+    );
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+    );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 import { Toaster } from "sonner";
+import { TanstackQueryProvider } from "./components/TanstackQueryProvider/TanstackQueryProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <Toaster position="top-right" closeButton />
-        <>Hello world</>
+        <TanstackQueryProvider>
+            <>Hello world</>
+        </TanstackQueryProvider>
     </React.StrictMode>
 );
