81e3bf7045b0bd39f60de335a1e01b5c70648743
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-10-09 11:13:53 -0300

    feat: add primary button

---
+++ src/components/Button/ButtonPrimary.tsx
@@ -0,0 +1,31 @@
+import { classNames } from "../../libs/classNames";
+
+interface Props {
+    type?: "button" | "submit" | "reset";
+    children: React.ReactNode;
+    className?: string;
+    onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+    title?: string;
+}
+
+export default function ButtonPrimary({
+    type,
+    children,
+    className,
+    onClick,
+    title,
+}: Props) {
+    return (
+        <button
+            type={type}
+            onClick={onClick}
+            className={classNames(
+                "uppercase font-light  p-2 rounded-md  transition-colors ease-in-out duration-500 text-white bg-blue-700 hover:bg-blue-600 drop-shadow",
+                className ? className : ""
+            )}
+            title={title ? title : ""}
+        >
+            {children}
+        </button>
+    );
+}
