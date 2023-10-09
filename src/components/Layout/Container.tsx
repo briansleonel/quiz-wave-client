edc389217953a95492425b9cba0f21c2eb5ce977
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-10-09 16:15:06 -0300

    styles: change component styles

--- src/components/Layout/Container.tsx
+++ src/components/Layout/Container.tsx
@@ -1,3 +1,3 @@
 export default function Container({ children }: { children: React.ReactNode }) {
-    return <div className="w-full min-h-[calc(100vh-4rem)]">{children}</div>;
+    return <div className="w-full min-h-[calc(100vh-4rem)] p-4 md:p-0 md:pl-16">{children}</div>;
 }
