61402d645f194d8e35c55bc3c3d9f2484a4bb80d
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-10-09 00:31:35 -0300

    feat: add sonner toast

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+import { Toaster } from "sonner";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
+        <Toaster position="top-right" closeButton />
         <>Hello world</>
     </React.StrictMode>
 );
