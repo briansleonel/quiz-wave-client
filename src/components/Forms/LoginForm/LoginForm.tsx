4ccee13021b4cff585faf51720672c482cc22744
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-10-10 23:03:33 -0300

    refractor: change to use react hook form

--- src/components/Forms/LoginForm/LoginForm.tsx
+++ src/components/Forms/LoginForm/LoginForm.tsx
@@ -2,56 +2,54 @@ import { PersonFill } from "react-bootstrap-icons";
 import { useLoginMutation } from "../../../hooks/users/useLogin";
 import ButtonPrimary from "../../Button/ButtonPrimary";
 import { toastInformation } from "../../Sonner/sonner.toast";
-import { useFormInput } from "../../../hooks/inputs/useFormInput";
-import { InputUnderline } from "../Input/InputUnderline";
+import { useForm, FormProvider } from "react-hook-form";
+import InputFormContext from "../Input/InputFormContext";
+
+interface Inputs {
+    username: string;
+    password: string;
+}
 
 export default function LoginForm() {
-    const inputUsername = useFormInput("");
-    const inputPassword = useFormInput("");
+    const formMethods = useForm<Inputs>();
 
     const loginMutation = useLoginMutation();
 
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        if (
-            inputUsername.inputProps.value !== "" &&
-            inputPassword.inputProps.value !== ""
-        ) {
-            await loginMutation.mutateAsync({
-                username: inputUsername.inputProps.value,
-                password: inputPassword.inputProps.value,
-            });
-        } else {
-            toastInformation("Debe ingresar sus credenciales");
+    const handleSubmit = formMethods.handleSubmit(
+        async (formData) => {
+            await loginMutation.mutateAsync(formData);
+        },
+        (err) => {
+            toastInformation("Ingrese sus credenciales");
         }
-    };
+    );
 
     return (
-        <form
-            method="POST"
-            onSubmit={handleSubmit}
-            className="max-w-sm p-8 bg-zinc-900 text-white rounded-md flex flex-col relative shadow-md drop-shadow-xl"
-        >
-            <div className="w-28 h-28 rounded-full bg-neutral-950 absolute -top-14 left-1/3 flex justify-center items-center ">
-                <PersonFill size="80%" />
-            </div>
-            <InputUnderline
-                name="username"
-                type="text"
-                placeholder="Nombre de usuario"
-                className="mt-8"
-                inputProps={inputUsername.inputProps}
-            />
+        <FormProvider {...formMethods}>
+            <form
+                method="POST"
+                onSubmit={handleSubmit}
+                className="max-w-sm p-8 bg-zinc-900 text-white rounded-md flex flex-col relative shadow-md drop-shadow-xl"
+            >
+                <div className="w-28 h-28 rounded-full bg-neutral-950 absolute -top-14 left-1/3 flex justify-center items-center ">
+                    <PersonFill size="80%" />
+                </div>
+                <InputFormContext
+                    name="username"
+                    type="text"
+                    placeholder="Nombre de usuario"
+                    className="mt-8"
+                />
 
-            <InputUnderline
-                name="password"
-                type="password"
-                placeholder="Contraseña"
-                className="mt-6"
-                inputProps={inputPassword.inputProps}
-            />
+                <InputFormContext
+                    name="password"
+                    type="password"
+                    placeholder="Contraseña"
+                    className="mt-6"
+                />
 
-            <ButtonPrimary className="mt-6">Ingresar</ButtonPrimary>
-        </form>
+                <ButtonPrimary className="mt-6">Ingresar</ButtonPrimary>
+            </form>
+        </FormProvider>
     );
 }
