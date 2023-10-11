a18ac25c9fa2a24d12607db485bec4ffa214b5a9
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-10-11 00:47:39 -0300

    feat: add alerts component

---
+++ src/components/Alerts/Alert.tsx
@@ -0,0 +1,19 @@
+import { classNames } from "../../libs/classNames";
+
+interface Props {
+    className?: string;
+    children: React.ReactNode;
+}
+
+export default function AlertDanger({ className, children }: Props) {
+    return (
+        <span
+            className={classNames(
+                "w-full bg-red-500 rounded-sm text-white text-normal p-4 text-center",
+                className ? className : ""
+            )}
+        >
+            {children}
+        </span>
+    );
+}
