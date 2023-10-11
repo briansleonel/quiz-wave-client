5e87ff6138bb82828d074311b2fd28b8ed4e9ba5
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-10-11 00:07:23 -0300

    feat: add modal dialog component

---
+++ src/components/Modals/ModalDialog.tsx
@@ -0,0 +1,82 @@
+import { Transition, Dialog } from "@headlessui/react";
+import { Fragment } from "react";
+import ButtonPrimary from "../Button/ButtonPrimary";
+import { XLg } from "react-bootstrap-icons";
+
+interface Props {
+    isOpen: boolean;
+    closeModal: () => void;
+    title: string;
+    children: React.ReactNode;
+    className?: string;
+    showBtnClose?: boolean;
+}
+
+export default function ModalDialog({
+    closeModal,
+    isOpen,
+    title,
+    children,
+    className,
+    showBtnClose,
+}: Props) {
+    return (
+        <Transition appear show={isOpen} as={Fragment}>
+            <Dialog as="div" className="relative z-50" onClose={() => {}}>
+                <Transition.Child
+                    as={Fragment}
+                    enter="ease-out duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in duration-200"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                >
+                    <div className="fixed inset-0 bg-black/60" />
+                </Transition.Child>
+
+                <div className="fixed z-50 inset-0 overflow-y-auto">
+                    <div className="flex min-h-full items-center justify-center p-2 text-center">
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0 scale-95"
+                            enterTo="opacity-100 scale-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100 scale-100"
+                            leaveTo="opacity-0 scale-95"
+                        >
+                            <Dialog.Panel
+                                className={`w-full md:max-w-md lg:max-w-lg transform overflow-hidden bg-white text-left align-middle transition-all rounded shadow-md shadow-gray-950/30 ${
+                                    className ? className : ""
+                                }`}
+                            >
+                                <div className="flex justify-between items-center border-b border-neutral-100">
+                                    <Dialog.Title
+                                        as="h3"
+                                        className="font-normal text-sm uppercase pl-2 text-gray-900"
+                                    >
+                                        {title}
+                                    </Dialog.Title>
+                                    {(showBtnClose ||
+                                        showBtnClose === undefined) && (
+                                        <ButtonPrimary
+                                            className="bg-white text-zinc-800 hover:bg-white hover:drop-shadow transition-all shadow-none drop-shadow-none text-lg"
+                                            onClick={closeModal}
+                                        >
+                                            <XLg />
+                                        </ButtonPrimary>
+                                    )}
+                                </div>
+
+                                <div className="p-4 flex flex-col items-center">
+                                    {children}
+                                </div>
+                            </Dialog.Panel>
+                        </Transition.Child>
+                    </div>
+                </div>
+            </Dialog>
+        </Transition>
+    );
+}
