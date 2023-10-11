628ac5df628079c6655d073436eedbfe297ceabd
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-10-11 00:08:20 -0300

    refractor: update category page

--- src/app/dashboard/category/CategoryPage.tsx
+++ src/app/dashboard/category/CategoryPage.tsx
@@ -1,3 +1,20 @@
+import ContainerUtil from "../../../components/Layout/ContainerUtil";
+import { Title } from "../../../components/Layout/TitleSubtitle";
+import ModalCategory from "../../../components/Modals/ModalCategory";
+import { Role } from "../../../libs/enums/role.enum";
+import { useAppSelector } from "../../../store/hooks.redux";
+
 export default function CategoryPage() {
-    return <>Category Page</>;
+    const { user } = useAppSelector((state) => state.auth);
+    return (
+        <ContainerUtil>
+            <Title>Categorías</Title>
+            {/*<CategoryForm />*/}
+            {user.role === Role.ADMIN && (
+                <div className="w-full flex justify-end">
+                    <ModalCategory edit={false} />
+                </div>
+            )}
+        </ContainerUtil>
+    );
 }
