192a184db9058c462e24d39de5622340e5410cbd
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-10-11 01:00:24 -0300

    feat: add custom hook to question mutations

---
+++ src/hooks/questions/useQuestion.ts
@@ -0,0 +1,107 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import questionService from "../../services/question.service";
+import { toastError, toastSuccess } from "../../components/Sonner/sonner.toast";
+import { useNavigate } from "react-router-dom";
+import { IQuestion, IQuestionId } from "../../types/question";
+
+const PATH_NAVIGATION = "/dashboard/question/";
+
+export function useDeleteQuestionMutation() {
+    const queryClient = useQueryClient();
+
+    const deleteQuestionMutation = useMutation({
+        mutationFn: questionService.deleteQuestion,
+        onSuccess: (data) => {
+            toastSuccess(data.message);
+            // Actualizar los datos después de eliminar un usuario
+            queryClient.invalidateQueries({ queryKey: ["questions"] });
+        },
+        onError: (err) => {
+            if (err instanceof Error) {
+                toastError(err.message);
+            }
+        },
+    });
+
+    return deleteQuestionMutation;
+}
+
+export function useChangeVerificationQuestion() {
+    const queryClient = useQueryClient();
+
+    const changeVerifiedQuestionMutation = useMutation({
+        mutationFn: questionService.changeVerification,
+        onSuccess: (data) => {
+            toastSuccess(data.message);
+            // Actualizar los datos después de eliminar un usuario
+            queryClient.invalidateQueries({ queryKey: ["questions"] });
+        },
+        onError: (err) => {
+            if (err instanceof Error) {
+                toastError(err.message);
+            }
+        },
+    });
+
+    return changeVerifiedQuestionMutation;
+}
+
+export function useUpdateQuestionMutation() {
+    const navigate = useNavigate();
+    const queryClient = useQueryClient();
+
+    const updateQuestionMutation = useMutation({
+        mutationFn: questionService.updateQuestion,
+
+        onSuccess: (data) => {
+            navigate(PATH_NAVIGATION);
+            toastSuccess(data.message as string);
+            queryClient.invalidateQueries({ queryKey: ["questions"] });
+        },
+        onError: (err) => {
+            if (err instanceof Error) {
+                toastError(err.message);
+            }
+        },
+    });
+
+    return updateQuestionMutation;
+}
+
+export function useAddQuestionMutation() {
+    const navigate = useNavigate();
+    const queryClient = useQueryClient();
+
+    const addQuestionMutation = useMutation({
+        mutationFn: questionService.addQuestion,
+
+        onSuccess: (data) => {
+            navigate(PATH_NAVIGATION);
+            toastSuccess(data.message as string);
+            queryClient.invalidateQueries({ queryKey: ["questions"] });
+        },
+        onError: (err) => {
+            if (err instanceof Error) {
+                toastError(err.message);
+            }
+        },
+    });
+
+    return addQuestionMutation;
+}
+
+export function useQuestion() {
+    // Mutations
+    const updateMutation = useUpdateQuestionMutation();
+    const addMutation = useAddQuestionMutation();
+
+    async function handlerUpdateQuestion(questionUpdate: IQuestionId) {
+        await updateMutation.mutateAsync(questionUpdate);
+    }
+
+    async function handlerAddQuestion(question: IQuestion) {
+        await addMutation.mutateAsync(question);
+    }
+
+    return { handlerAddQuestion, handlerUpdateQuestion };
+}
