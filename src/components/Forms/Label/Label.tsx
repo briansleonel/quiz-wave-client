87ddbe2b8421f0705d032b2a35806acbe88a84d1
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-10-11 00:05:17 -0300

    feat: add label component

---
+++ src/components/Forms/Label/Label.tsx
@@ -0,0 +1,24 @@
+import { classNames } from "../../../libs/classNames";
+
+interface Props {
+    label: string;
+    name: string;
+    children: React.ReactNode;
+    className?: string;
+}
+
+export default function Label({ children, label, name, className }: Props) {
+    return (
+        <div className="w-full">
+            <div
+                className={classNames(
+                    "text-start mb-1 w-full ",
+                    className ?? ""
+                )}
+            >
+                <label htmlFor={name}>{label}</label>
+            </div>
+            {children}
+        </div>
+    );
+}
