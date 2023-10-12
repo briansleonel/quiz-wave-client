1a045c27057397ceaf5fca48bbeece56c9a1faa3
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-10-11 22:54:39 -0300

    feat: add filters question to question page

--- src/app/dashboard/question/QuestionPage.tsx
+++ src/app/dashboard/question/QuestionPage.tsx
@@ -1,3 +1,12 @@
+import FiltersQuestion from "../../../components/Filter/FiltersQuestion";
+import ContainerUtil from "../../../components/Layout/ContainerUtil";
+import { Title } from "../../../components/Layout/TitleSubtitle";
+
 export default function QuestionPage() {
-    return <>Question Page</>;
+    return (
+        <ContainerUtil>
+            <Title>Preguntas</Title>
+            <FiltersQuestion />
+        </ContainerUtil>
+    );
 }
