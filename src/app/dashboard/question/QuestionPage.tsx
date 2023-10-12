84f1398515c2991bf7e5def33293a7662c973fa9
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-10-11 23:28:09 -0300

    feat: add table question to component

--- src/app/dashboard/question/QuestionPage.tsx
+++ src/app/dashboard/question/QuestionPage.tsx
@@ -1,12 +1,14 @@
 import FiltersQuestion from "../../../components/Filter/FiltersQuestion";
 import ContainerUtil from "../../../components/Layout/ContainerUtil";
 import { Title } from "../../../components/Layout/TitleSubtitle";
+import TableQuestions from "../../../components/Table/table-question/TableQuestion";
 
 export default function QuestionPage() {
     return (
         <ContainerUtil>
             <Title>Preguntas</Title>
             <FiltersQuestion />
+            <TableQuestions />
         </ContainerUtil>
     );
 }
