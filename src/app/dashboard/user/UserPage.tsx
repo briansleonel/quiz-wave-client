8dd00d17e2fd9594c78d63108aa7c8f085417da6
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-10-12 22:41:02 -0300

    feat: add components to display users

--- src/app/dashboard/user/UserPage.tsx
+++ src/app/dashboard/user/UserPage.tsx
@@ -1,3 +1,14 @@
+import Filters from "../../../components/Filter/Filters";
+import ContainerUtil from "../../../components/Layout/ContainerUtil";
+import { Title } from "../../../components/Layout/TitleSubtitle";
+import TableUsers from "../../../components/Table/table-user/TableUsers";
+
 export default function UserPage() {
-    return <>User Page</>;
+    return (
+        <ContainerUtil>
+            <Title>Usuarios</Title>
+            <Filters />
+            <TableUsers />
+        </ContainerUtil>
+    );
 }
