e8869b453bbc42066f6df4a1b6bb23727bb3c909
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-10-12 23:29:29 -0300

    feat: add libs to question

---
+++ src/libs/questions.libs.ts
@@ -0,0 +1,67 @@
+import { IQuestion, IQuestionId } from "../types/question";
+import { IQuestionCategory } from "../types/questionCategory";
+import { loadStateAuthLocalStorage } from "./state.localstorage";
+
+export function convertToQuestion({
+    category,
+    correctOption,
+    options,
+    question,
+    description,
+    user,
+}: {
+    category: IQuestionCategory;
+    question: string;
+    options: Array<string>;
+    correctOption: string;
+    description: string;
+    user?: string;
+}) {
+    const newQuestion: IQuestion = {
+        category: category,
+        question: question,
+        description: description,
+        options: orderOptions(options, correctOption),
+        correct: 0,
+        user: user ? user : loadStateAuthLocalStorage()!._id,
+    };
+
+    return newQuestion;
+}
+
+export function convertToQuestionwithId({
+    id,
+    category,
+    correctOption,
+    options,
+    question,
+    description,
+    user,
+}: {
+    id: string;
+    category: IQuestionCategory;
+    question: string;
+    options: Array<string>;
+    correctOption: string;
+    description: string;
+    user: string;
+}) {
+    const newQuestion: IQuestionId = {
+        _id: id,
+        category: category,
+        question: question,
+        description: description,
+        options: orderOptions(options, correctOption),
+        correct: 0,
+        user: user,
+    };
+
+    return newQuestion;
+}
+
+export function orderOptions(options: Array<string>, correct: string) {
+    const ordered = [correct];
+    const othersOptions = options.filter((opt) => opt !== correct);
+    ordered.push(...othersOptions);
+    return ordered;
+}
