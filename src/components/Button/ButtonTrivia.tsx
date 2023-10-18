660c82855d2ff4d1e0b6643dedd844ecb89b14ab
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-10-18 14:55:29 -0300

    feat: add button component for game routes

---
+++ src/components/Button/ButtonTrivia.tsx
@@ -0,0 +1,28 @@
+import { MouseEvent } from "react";
+import { classNames } from "../../libs/classNames";
+
+export default function ButtonTrivia({
+    onClickFn,
+    children,
+    title,
+    className,
+}: {
+    onClickFn: (e: MouseEvent<HTMLButtonElement>) => void;
+    children: React.ReactNode;
+    title: string;
+    className?: string;
+    //type: 'submit' | 'reset' | 'button' | undefined;
+}) {
+    return (
+        <button
+            className={classNames(
+                "rounded bg-purple-600 text-white hover:text-white shadow shadow-neutral-900 transition-all duration-500 mx-auto px-12 py-4 font-bold text-xl tracking-widest uppercase",
+                className ? className : ""
+            )}
+            onClick={onClickFn}
+            title={title}
+        >
+            {children}
+        </button>
+    );
+}
