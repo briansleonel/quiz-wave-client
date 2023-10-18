3f6c47f3f62ec56281e98a1111e9723fed49374b
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-10-18 14:59:16 -0300

    styles: change component styles

--- src/components/Layout/Container.tsx
+++ src/components/Layout/Container.tsx
@@ -1,3 +1,16 @@
+import { useLocation } from "react-router-dom";
+
 export default function Container({ children }: { children: React.ReactNode }) {
-    return <div className="w-full min-h-[calc(100vh-4rem)] p-4 md:p-0 md:pl-16">{children}</div>;
+    const location = useLocation();
+    return (
+        <div
+            className={`w-full min-h-[calc(100vh-4rem)] ${
+                location.pathname.startsWith("/trivia")
+                    ? ""
+                    : "p-4 md:p-0 md:pl-16"
+            }`}
+        >
+            {children}
+        </div>
+    );
 }
