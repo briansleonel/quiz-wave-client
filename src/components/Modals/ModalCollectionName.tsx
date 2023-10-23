2ae019ddde5194e715e1915bbfcb9650f717e065
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-10-23 17:03:21 -0300

    refractor: implements event to cancel title and description editing

--- src/components/Modals/ModalCollectionName.tsx
+++ src/components/Modals/ModalCollectionName.tsx
@@ -1,10 +1,9 @@
-import { Pencil } from "react-bootstrap-icons";
+import { InputCursorText } from "react-bootstrap-icons";
 import useModal from "../../hooks/modal/useModal";
 import ButtonPrimary from "../Button/ButtonPrimary";
 import ModalDialog from "./ModalDialog";
 import Label from "../Forms/Label/Label";
 import { Input } from "../Forms/Input/Input";
-import { InputPropType, InputTextAreaPropType } from "../../types/util";
 import { TextArea } from "../Forms/Input/TextArea";
 import { useFormInput, useFormTextArea } from "../../hooks/inputs/useFormInput";
 
@@ -35,21 +34,26 @@ export default function ModalCollectionName({
         closeModal();
     };
 
+    const handleCancel = () => {
+        inputTitle.setInput(title);
+        inputDescription.setInput(description);
+        closeModal();
+    };
+
     return (
         <>
             <ButtonPrimary
                 className="bg-transparent !text-neutral-600 text-lg hover:bg-transparent hover:!text-neutral-800 hover:shadow"
-                title="Editar nombre y descripción"
+                title="Renombrar"
                 onClick={openModal}
             >
-                <Pencil />
+                <InputCursorText />
             </ButtonPrimary>
 
             <ModalDialog
-                closeModal={closeModal}
+                closeModal={handleCancel}
                 isOpen={showModal}
                 title=""
-                showBtnClose={false}
             >
                 <form className="w-full grid grid-cols-1 gap-4">
                     <Label label="Título" name="name">
@@ -70,13 +74,13 @@ export default function ModalCollectionName({
                     <div className="flex flex-col md:flex-row items-center justify-center gap-2 w-full mt-6">
                         <ButtonPrimary
                             className="text-sm font-normal w-full bg-neutral-200 !text-black hover:bg-neutral-300"
-                            onClick={closeModal}
+                            onClick={handleCancel}
                             type="button"
                         >
                             Cancelar
                         </ButtonPrimary>
                         <ButtonPrimary
-                            className="text-sm font-normal w-full"
+                            className="text-sm font-normal w-full bg-green-500 hover:bg-green-600"
                             onClick={handleSubmit}
                         >
                             Listo
