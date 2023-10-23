8e77e27327c0b66ce2fcb40cab596fd1c5fd1f80
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-10-22 23:43:31 -0300

    feat: update collection ABM page

--- src/app/dashboard/collection/CollectionABMPage.tsx
+++ src/app/dashboard/collection/CollectionABMPage.tsx
@@ -1,64 +1,47 @@
 import { useParams } from "react-router-dom";
-import { ICollectionQuestion } from "../../../types/collection";
-
-const pregsMock: Array<ICollectionQuestion> = [
-    {
-        duration: 20,
-        question: {
-            _id: "64c07d74479f3c5d5c2b1eea",
-            question: "¿Cuál es el hueso más pequeño del cuerpo?",
-            options: [
-                "El estribo",
-                "La falange",
-                "El yunque",
-                "Ninguna es correcta",
-            ],
-            correct: 0,
-            category: {
-                _id: "6490d5b75081691a8ac04056",
-                name: "ciencia",
-            },
-            user: "6495c8caac05cd47cf02b0c7",
-            verified: true,
-            description: "",
-        },
-    },
-    {
-        duration: 20,
-        question: {
-            _id: "64c07d74479f3c5d5c2b1eea",
-            question: "¿Qué estudia la icitología?",
-            options: [
-                "Los peces",
-                "Los insectos",
-                "Las erupciones cutáneas",
-                "Las rocas",
-            ],
-            correct: 0,
-            verified: true,
-            category: {
-                _id: "6490d5b75081691a8ac04056",
-                name: "ciencia",
-            },
-            user: "6495c8caac05cd47cf02b0c7",
-            description: "",
-        },
-    },
-];
+import ContainerUtil from "../../../components/Layout/ContainerUtil";
+import ButtonPrimary from "../../../components/Button/ButtonPrimary";
+import { useState } from "react";
+import { CircleFill } from "react-bootstrap-icons";
+import { ICollectionWithUpdatedAt } from "../../../types/collection";
+import ModalCollectionName from "../../../components/Modals/ModalCollectionName";
 
 export default function CollectionABMPage() {
     const params = useParams<{ id: string }>();
 
+    const [collection, setCollection] = useState<ICollectionWithUpdatedAt>();
+
+    const changeTitleAndDescription = (title: string, description: string) => {
+        setCollection({ ...collection!, name: title, description });
+    };
+
     return (
-        <>
-            <section id="showQuestions">
-                {pregsMock.map((questionColl, index) => (
-                    <div>
-                        <span>#{index + 1}</span>
-                        <span>{questionColl.question.question}</span>
+        <ContainerUtil>
+            <section className="w-full grid grid-cols-3 min-h-max gap-8">
+                <div className="flex flex-col gap-4 bg-white rounded p-6 shadow-md">
+                    <div className="flex items-start justify-between gap-2">
+                        <h1 className="text-2xl font-medium">
+                            {collection?.name ?? "Titulo de la colección"}
+                        </h1>
+                        <ModalCollectionName
+                            description={collection?.description ?? ""}
+                            title={collection?.name ?? ""}
+                            changeTitleAndDescription={
+                                changeTitleAndDescription
+                            }
+                        />
                     </div>
-                ))}
+
+                    <ButtonPrimary className="text-sm font-medium tracking-wider">
+                        Empezar
+                    </ButtonPrimary>
+
+                    <span className="text-sm text-neutral-500 flex items-center gap-2">
+                        <CircleFill className="w-2" /> Actualizado 2 semanas
+                    </span>
+                </div>
+                <div className="col-span-2">Questions</div>
             </section>
-        </>
+        </ContainerUtil>
     );
 }
