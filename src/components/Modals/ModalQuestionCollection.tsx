d426ce3919fcc249a2b396773f5ddeb5f39a772e
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-10-29 15:19:18 -0300

    feat: update component to add a question collection

--- src/components/Modals/ModalQuestionCollection.tsx
+++ src/components/Modals/ModalQuestionCollection.tsx
@@ -1,37 +1,27 @@
 import { useFormInput, useFormTextArea } from "../../hooks/inputs/useFormInput";
 import useModal from "../../hooks/modal/useModal";
 import ButtonPrimary from "../Button/ButtonPrimary";
-import { Input } from "../Forms/Input/Input";
-import Label from "../Forms/Label/Label";
 import ModalDialog from "./ModalDialog";
-import InputOption from "../Forms/Input/InputOption";
-import OptionsButtonGroup from "../Forms/QuestionForm/OptionsButtonGroup";
 import { useQuestionOption } from "../../hooks/questions/useQuestionOptions";
-import { TextArea } from "../Forms/Input/TextArea";
 import { ICollectionQuestionWithId } from "../../types/question";
 import { toastError } from "../Sonner/sonner.toast";
 import { orderOptions } from "../../libs/questions.libs";
+import QuestionCollectionForm from "../Forms/QuestionCollectionForm/QuestionCollectionForm";
 
 interface Props {
-    question?: ICollectionQuestionWithId;
     questions: Array<ICollectionQuestionWithId>;
     setQuestions: (questions: Array<ICollectionQuestionWithId>) => void;
 }
 
 export default function ModalQuestionCollection({
-    question,
     questions,
     setQuestions,
 }: Props) {
     const { closeModal, openModal, showModal } = useModal();
 
-    const inputQuestion = useFormInput(question ? question.question : "");
-    const inputDuration = useFormInput(
-        question ? question.duration.toString() : "20"
-    );
-    const inputDescription = useFormTextArea(
-        question && question.description ? question.description : ""
-    );
+    const inputQuestion = useFormInput("");
+    const inputDuration = useFormInput("20");
+    const inputDescription = useFormTextArea("");
 
     const {
         options,
@@ -40,13 +30,13 @@ export default function ModalQuestionCollection({
         correctOption,
         setCorrectOption,
         setOptions,
-        handlerAddOption,
-        handlerCancelEditOption,
-        handlerDeleteOption,
-        handlerEditOption,
+        handleAddOption,
+        handleCancelEditOption,
+        handleDeleteOption,
+        handleEditOption,
     } = useQuestionOption({
-        optionsGroup: question ? question.options : [],
-        correct: question ? question.options[question.correct] : "",
+        optionsGroup: [],
+        correct: "",
     });
 
     /**
@@ -61,39 +51,19 @@ export default function ModalQuestionCollection({
         if (inputQuestion.inputProps.value) {
             if (options.length >= 2) {
                 if (correctOption !== "") {
-                    if (question) {
-                        /*
-                        const updateQuestion = convertToQuestionCollection({
-                            _id: question._id,
-                            question: inputQuestion.inputProps.value,
-                            description: inputDescription.inputProps.value,
-                            options,
-                            correctOption,
-                            duration: Number(inputDuration.inputProps.value),
-                        });
-                        */
-                        question.options = orderOptions(options, correctOption);
-                        question.description =
-                            inputDescription.inputProps.value;
-                        question.question = inputQuestion.inputProps.value;
-                        question.duration = Number(
-                            inputDuration.inputProps.value
-                        );
-                        question.correct = 0;
+                    const newQuestion: ICollectionQuestionWithId = {
+                        _id: "",
+                        question: inputQuestion.inputProps.value,
+                        description: inputDescription.inputProps.value,
+                        options: orderOptions(options, correctOption),
+                        correct: 0,
+                        duration: Number(inputDuration.inputProps.value),
+                    };
 
-                        handleCancel();
-                    } else {
-                        const newQuestion: ICollectionQuestionWithId = {
-                            _id: "",
-                            question: inputQuestion.inputProps.value,
-                            description: inputDescription.inputProps.value,
-                            options: orderOptions(options, correctOption),
-                            correct: 0,
-                            duration: Number(inputDuration.inputProps.value),
-                        };
-                        setQuestions([...questions, newQuestion]);
-                        handleCancel();
-                    }
+                    setQuestions([...questions, newQuestion]);
+
+                    cleanInputs();
+                    closeModal();
                 } else {
                     toastError("Debe seleccionar una opción como correcta");
                 }
@@ -103,11 +73,6 @@ export default function ModalQuestionCollection({
         } else {
             toastError("Debe ingresar una pregunta");
         }
-    };
-
-    const handleCancel = () => {
-        cleanInputs();
-        closeModal();
     };
 
     const cleanInputs = () => {
@@ -128,75 +93,23 @@ export default function ModalQuestionCollection({
                 Agregar
             </ButtonPrimary>
 
-            <ModalDialog closeModal={handleCancel} isOpen={showModal} title="">
-                <form className="w-full grid grid-cols-1 gap-4">
-                    <Label label="Pregunta" name="question">
-                        <Input
-                            type="text"
-                            name="question"
-                            inputProps={inputQuestion.inputProps}
-                        />
-                    </Label>
-
-                    {/** INPUT OPTIONS */}
-                    <Label
-                        label={`${editOption ? "Editar" : "Nueva"} opción`}
-                        name="option"
-                    >
-                        <InputOption
-                            editOption={editOption}
-                            handleAdd={handlerAddOption}
-                            handleCancelEdit={handlerCancelEditOption}
-                            inputProps={inputOptions.inputProps}
-                        />
-                    </Label>
-
-                    {/** OPTIONS */}
-                    {options.length > 0 && (
-                        <Label label="Opciones" name="options">
-                            <div className="p-2 bg-neutral-50 rounded border">
-                                <OptionsButtonGroup
-                                    correctOption={correctOption}
-                                    handleDelete={handlerDeleteOption}
-                                    handleEdit={handlerEditOption}
-                                    options={options}
-                                    setCorrectOption={setCorrectOption}
-                                />
-                            </div>
-                        </Label>
-                    )}
-
-                    <Label label="Duración" name="duration">
-                        <Input
-                            type="number"
-                            name="duration"
-                            inputProps={inputDuration.inputProps}
-                        />
-                    </Label>
-
-                    <Label label="Descripción" name="description">
-                        <TextArea
-                            name="description"
-                            inputProps={inputDescription.inputProps}
-                        />
-                    </Label>
-
-                    <div className="flex flex-col md:flex-row items-center justify-center gap-2 w-full mt-6">
-                        <ButtonPrimary
-                            className="text-sm font-normal w-full bg-neutral-200 !text-black hover:bg-neutral-300"
-                            onClick={handleCancel}
-                            type="button"
-                        >
-                            Cancelar
-                        </ButtonPrimary>
-                        <ButtonPrimary
-                            className="text-sm font-normal w-full bg-green-500 hover:bg-green-600"
-                            onClick={handleSubmit}
-                        >
-                            Listo
-                        </ButtonPrimary>
-                    </div>
-                </form>
+            <ModalDialog closeModal={closeModal} isOpen={showModal} title="">
+                <QuestionCollectionForm
+                    correctOption={correctOption}
+                    editOption={editOption}
+                    handleCancel={closeModal}
+                    handleSubmit={handleSubmit}
+                    handleAddOption={handleAddOption}
+                    handleCancelEditOption={handleCancelEditOption}
+                    handleDeleteOption={handleDeleteOption}
+                    handleEditOption={handleEditOption}
+                    inputDescriptionProps={inputDescription.inputProps}
+                    inputDurationProps={inputDuration.inputProps}
+                    inputOptionsProps={inputOptions.inputProps}
+                    inputQuestionProps={inputQuestion.inputProps}
+                    options={options}
+                    setCorrectOption={setCorrectOption}
+                />
             </ModalDialog>
         </>
     );
