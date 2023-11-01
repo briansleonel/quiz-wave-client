36cb29a712031293fc15ef8712b70604c239c5b8
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-11-01 00:10:51 -0300

    feat: implement service to get collections

--- src/app/dashboard/collection/CollectionPage.tsx
+++ src/app/dashboard/collection/CollectionPage.tsx
@@ -1,23 +1,42 @@
-import { Pencil, Trash, Trash3 } from "react-bootstrap-icons";
-import ButtonPrimary from "../../../components/Button/ButtonPrimary";
 import ContainerUtil from "../../../components/Layout/ContainerUtil";
-import { collectionsMock } from "../../../services/collecttions.mock";
-import { getTimeAgo } from "../../../libs/getTimeAgo";
 import CollectionCard from "../../../components/Cards/CollectionCard";
-import { Title } from "../../../components/Layout/TitleSubtitle";
+import { useQuery } from "@tanstack/react-query";
+import collectionService from "../../../services/collection.service";
+import AlertDanger from "../../../components/Alerts/Alert";
+import { useEffect, useState } from "react";
+import { ApiPagination } from "../../../types/api";
 
 export default function CollectionPage() {
+    // datos de paginación recibidos por la api
+    const [pagination, setPagination] = useState<ApiPagination>();
+
+    // react-query para la obtención de datos desde la api
+    const { data, isLoading, error, isFetching } = useQuery({
+        queryKey: ["collections", pagination],
+        queryFn: () =>
+            collectionService.getCollectionsPagination({
+                limit: pagination?.limit || 10,
+                page: pagination?.page || 1,
+            }),
+    });
+
     return (
         <>
             <ContainerUtil>
-                <section className="w-full grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8">
-                    {collectionsMock.map((collection) => (
-                        <CollectionCard
-                            key={collection._id}
-                            collection={collection}
-                        />
-                    ))}
-                </section>
+                {error && error instanceof Error ? (
+                    <AlertDanger>Error: {error.message}</AlertDanger>
+                ) : isLoading ? (
+                    <div>Caargando...</div>
+                ) : data ? (
+                    <section className="w-full grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8">
+                        {data.data.map((collection) => (
+                            <CollectionCard
+                                key={collection._id}
+                                collection={collection}
+                            />
+                        ))}
+                    </section>
+                ) : null}
             </ContainerUtil>
         </>
     );
