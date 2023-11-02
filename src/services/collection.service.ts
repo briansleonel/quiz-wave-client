2290c17fb93620e276e5e97d10397788fa49bcb6
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-11-02 00:02:38 -0300

    feat: add service to get a collection by id

--- src/services/collection.service.ts
+++ src/services/collection.service.ts
@@ -1,12 +1,12 @@
 import { isAxiosError } from "axios";
 import { __instanceAxios, endpointsAPI } from "../config/config";
 import { APIResponse, PaginationFetch } from "../types/api";
-import { ICollection, ICollectionWithUpdatedAt } from "../types/collection";
+import { ICollectionWithUpdatedAt } from "../types/collection";
 
 export interface QueryFetchCollection extends PaginationFetch {}
 
 /**
- * Permite envíar una petición GET a "/api/category/all" para devolver todas las categorías almacenadas en la API, sin paginación de datos
+ * Permite envíar una petición GET a "/api/collection/" para devolver todas las collections almacenadas en la API, con paginación de datos
  *
  * @returns todas las categorias
  */
@@ -17,7 +17,6 @@ async function getCollectionsPagination({ limit, page }: QueryFetchCollection) {
         const response = await __instanceAxios.get(
             endpointsAPI.COLLECTION + paginationData
         );
-        console.log(response.data);
 
         return response.data as APIResponse<Array<ICollectionWithUpdatedAt>>;
     } catch (error) {
@@ -33,8 +32,38 @@ async function getCollectionsPagination({ limit, page }: QueryFetchCollection) {
     }
 }
 
+/**
+ * Permite enviar una petición GET a '/api/collection/:id' y devolver una determinada collection, de acuerdo a su ID
+ * @param id idetntificador de collection
+ * @returns
+ */
+async function getCollection(id: string) {
+    try {
+        const response = await __instanceAxios.get(
+            `${endpointsAPI.COLLECTION}/${id}`
+        );
+
+        return response.data as APIResponse<ICollectionWithUpdatedAt>;
+    } catch (error) {
+        // Si el error es una instancia de AxiosError, puedes acceder a la propiedad response
+        if (isAxiosError(error)) {
+            //Si la respuesta tiene un código de estado, significa que la API respondió con un error HTTP
+            if (error.response) {
+                // Lanza una excepción con el mensaje de error recibido de la API
+                throw new Error(error.response.data.message);
+            } else {
+                throw new Error(error.message);
+            }
+        } else {
+            // Si no hay respuesta o no se pudo conectar con la API, lanza una excepción genérica
+            throw new Error("Error sin procesar");
+        }
+    }
+}
+
 const collectionService = {
     getCollectionsPagination,
+    getCollection,
 };
 
 export default collectionService;
