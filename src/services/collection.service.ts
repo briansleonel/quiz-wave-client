2ea4566e5e2aa6eb68325703682f782cbb85b684
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-11-02 15:42:09 -0300

    feat: add service to update a collection

--- src/services/collection.service.ts
+++ src/services/collection.service.ts
@@ -1,7 +1,11 @@
 import { isAxiosError } from "axios";
 import { __instanceAxios, endpointsAPI } from "../config/config";
 import { APIResponse, PaginationFetch } from "../types/api";
-import { ICollection, ICollectionWithUpdatedAt } from "../types/collection";
+import {
+    ICollection,
+    ICollectionWithId,
+    ICollectionWithUpdatedAt,
+} from "../types/collection";
 
 export interface QueryFetchCollection extends PaginationFetch {}
 
@@ -85,10 +89,35 @@ async function addCollection(collection: ICollection) {
     }
 }
 
+async function updateCollection(collection: ICollectionWithId) {
+    try {
+        const response = await __instanceAxios.put(
+            `${endpointsAPI.COLLECTION}/${collection._id}`,
+            collection
+        );
+        return response.data as APIResponse<ICollectionWithId>;
+    } catch (error) {
+        // Si el error es una instancia de AxiosError, puedes acceder a la propiedad response
+        if (isAxiosError(error)) {
+            //Si la respuesta tiene un código de estado, significa que la API respondió con un error HTTP
+            if (error.response) {
+                // Lanza una excepción con el mensaje de error recibido de la API
+                throw new Error(error.response.data.message);
+            } else {
+                throw new Error(error.message);
+            }
+        } else {
+            // Si no hay respuesta o no se pudo conectar con la API, lanza una excepción genérica
+            throw new Error("Error sin procesar");
+        }
+    }
+}
+
 const collectionService = {
     getCollectionsPagination,
     getCollection,
     addCollection,
+    updateCollection,
 };
 
 export default collectionService;
