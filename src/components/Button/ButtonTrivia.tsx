63e4389e65b8748088d5514b6c5a16e93d49f3e8
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-11-10 23:55:20 -0300

    styles: change styles of component

--- src/components/Button/ButtonTrivia.tsx
+++ src/components/Button/ButtonTrivia.tsx
@@ -9,18 +9,18 @@ export default function ButtonTrivia({
 }: {
     onClickFn: (e: MouseEvent<HTMLButtonElement>) => void;
     children: React.ReactNode;
-    title: string;
+    title?: string;
     className?: string;
     //type: 'submit' | 'reset' | 'button' | undefined;
 }) {
     return (
         <button
             className={classNames(
-                "rounded bg-purple-600 text-white hover:text-white shadow shadow-neutral-900 transition-all duration-500 mx-auto px-12 py-4 font-bold text-xl tracking-widest uppercase",
+                "rounded bg-purple-600 text-neutral-50 hover:text-neutral-50 shadow shadow-neutral-800 transition-all duration-500 mx-auto px-4 py-2 font-medium text-lg uppercase",
                 className ? className : ""
             )}
             onClick={onClickFn}
-            title={title}
+            title={title ?? ""}
         >
             {children}
         </button>
