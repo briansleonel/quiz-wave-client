499df6b77e059c423b431a9813190d8a40b78ae5
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-11-12 20:24:32 -0300

    styles: change styles of component

--- src/app/lobby/LobbyPage.tsx
+++ src/app/lobby/LobbyPage.tsx
@@ -31,18 +31,18 @@ export default function LobbyPage() {
         <BackgroundQuiz>
             <main className="w-full h-screen flex flex-col items-center justify-start gap-36 p-20 md:px-44">
                 {/** Mostrar el código de juego */}
-                <section className="flex w-1/4 rounded transform -skew-x-12 bg-indigo-600 shadow-md shadow-neutral-900">
-                    <div className="w-1/4 rounded py-4 px-6  bg-indigo-800">
-                        <div className="transform skew-x-12 ">
-                            <h3 className="text-3xl text-center text-white font-bold">
+                <section className="flex w-fit rounded transform -skew-x-12 bg-indigo-600 shadow-md shadow-neutral-900 text-5xl">
+                    <div className="rounded py-4 px-8  bg-indigo-800">
+                        <div className="transform skew-x-12">
+                            <h3 className="text-center text-white font-medium">
                                 PIN
                             </h3>
                         </div>
                     </div>
-                    <div className="w-full rounded py-4 px-6 bg-indigo-600 ">
+                    <div className="w-full rounded py-4 px-16 bg-indigo-600 ">
                         <div className="transform skew-x-12 ">
-                            <h3 className="text-3xl transform text-center text-neutral-300 font-bold">
-                                PIN
+                            <h3 className="transform text-center text-neutral-200 font-semibold tracking-wide">
+                                123456
                             </h3>
                         </div>
                     </div>
@@ -60,7 +60,7 @@ export default function LobbyPage() {
                     ))}
                 </section>
 
-                <span className="font-bold text-neutral-200 text-3xl transform -skew-x-12">
+                <span className="font-medium text-neutral-200 text-4xl transform -skew-x-12">
                     Esperando jugadores...
                 </span>
             </main>
