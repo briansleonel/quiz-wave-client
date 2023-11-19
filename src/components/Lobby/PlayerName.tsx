de8e63d780250ca2225a94c9d850190f26b2b37b
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-11-19 18:20:12 -0300

    styles: change styles component

--- src/components/Lobby/PlayerName.tsx
+++ src/components/Lobby/PlayerName.tsx
@@ -8,7 +8,7 @@ export default function PlayerName({ player }: Props) {
     return (
         <span
             key={player.socketId}
-            className="w-full flex items-center justify-center rounded-sm bg-white p-3 text-center text-2xl font-semibold"
+            className="w-full flex items-center justify-center rounded-sm bg-emerald-600 text-neutral-50 p-3 text-center text-3xl font-semibold shadow-2xl"
         >
             {player.name}
         </span>
--- src/components/Lobby/RoomCode.tsx
+++ src/components/Lobby/RoomCode.tsx
@@ -4,15 +4,15 @@ interface Props {
 
 export default function RoomCode({ code }: Props) {
     return (
-        <section className="flex w-fit rounded transform -skew-x-12 bg-indigo-600 shadow-md shadow-neutral-900 text-5xl">
-            <div className="rounded py-4 px-8  bg-indigo-800">
-                <div className="transform skew-x-12">
-                    <h3 className="text-center text-white font-medium">PIN</h3>
+        <section className="flex w-fit rounded transform -skew-x-12 bg-indigo-600 shadow-xl shadow-neutral-950 text-7xl">
+            <div className="rounded py-4 px-8  bg-indigo-800 flex items-center justify-center">
+                <div className="transform skew-x-12 flex items-center justify-center">
+                    <span className="text-center text-white font-medium text-2xl">PIN de juego:</span>
                 </div>
             </div>
             <div className="w-full rounded py-4 px-16 bg-indigo-600 ">
                 <div className="transform skew-x-12 ">
-                    <h3 className="transform text-center text-neutral-200 font-semibold tracking-wide">
+                    <h3 className="transform text-center text-neutral-200 font-extrabold tracking-widest">
                         {code}
                     </h3>
                 </div>
