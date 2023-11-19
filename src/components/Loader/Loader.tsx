9c5f3843058c3e9b55e2141a87038e620a5a1ae8
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-11-19 17:24:23 -0300

    styles: change component styles

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -2,11 +2,12 @@ import styles from "./loader.module.css";
 
 interface Props {
     style?: "black" | "indigo";
+    className?: string;
 }
 
-export default function Loader({ style }: Props) {
+export default function Loader({ style, className }: Props) {
     return (
-        <div className={styles.containerLoader}>
+        <div className={`${styles.containerLoader} ${className}`}>
             <span
                 className={`${styles.elementLoader} ${
                     style === "black" ? styles.elementLoaderBlack : ""
