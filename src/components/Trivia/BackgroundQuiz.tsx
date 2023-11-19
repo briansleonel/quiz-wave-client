a91eaa834fc908109c18e239653f157970bca052
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-11-19 17:26:06 -0300

    styles: change background image

--- src/components/Trivia/BackgroundQuiz.tsx
+++ src/components/Trivia/BackgroundQuiz.tsx
@@ -4,8 +4,10 @@ export default function BackgroundQuiz({
     children: React.ReactNode;
 }) {
     return (
-        <div className="w-full min-h-screen flex justify-center items-center bg-backgroundQuiz bg-no-repeat bg-cover bg-fixed">
-            {children}
+        <div className="w-full min-h-screen flex justify-center items-center bg-[url('/room-1.png')] bg-no-repeat bg-cover bg-fixed">
+            <div className="w-full h-full flex  justify-center items-center backdrop-brightness-50">
+                {children}
+            </div>
         </div>
     );
 }
