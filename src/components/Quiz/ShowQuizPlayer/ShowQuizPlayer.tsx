03dfc8a2469895d756c37db002c10bf9da323540
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-12-21 23:58:33 -0300

    feat: show options to player

--- src/components/Quiz/ShowQuizPlayer/ShowQuizPlayer.tsx
+++ src/components/Quiz/ShowQuizPlayer/ShowQuizPlayer.tsx
@@ -1,23 +1,36 @@
 import { useEffect, useState } from "react";
 import { socket } from "../../../socket";
+import { optionsModel } from "../../OptionsModel/OptionsModel";
 
 export default function ShowQuizPlayer() {
     const [countdown, setCountdown] = useState<number>();
     const [question, setQuestion] = useState<string>();
+    const [options, setOptions] = useState<Array<string>>();
 
     const [showOptions, setShowOptions] = useState(false);
 
     useEffect(() => {
-        socket.on("quiz:show-question", (question) => {
+        function showQuestionEvent(question: string) {
             setQuestion(question);
-        });
+        }
 
-        socket.on("quiz:countdown", (countdown) => {
+        function showOptionsEvent(opts: Array<string>) {
+            setOptions(opts);
+            setShowOptions(true);
+        }
+
+        function updateCountodwn(countdown: number) {
             setCountdown(countdown);
-        });
+        }
+
+        socket.on("quiz:show-question", showQuestionEvent);
+        socket.on("quiz:countdown", updateCountodwn);
+        socket.on("quiz:show-options", showOptionsEvent);
 
         return () => {
-            socket.off("quiz:show-question");
+            socket.off("quiz:show-question", showQuestionEvent);
+            socket.off("quiz:show-options", showOptionsEvent);
+            socket.off("quiz:countdown", updateCountodwn);
         };
     });
 
@@ -49,6 +62,20 @@ export default function ShowQuizPlayer() {
                             <span className="text-white text-5xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 block">
                                 {countdown}
                             </span>
+                        </div>
+                    )}
+
+                    {showOptions && options && (
+                        <div className="p-4 grid grid-cols-2 gap-4">
+                            {options.map((o, i) => (
+                                <div
+                                    key={i}
+                                    className={`text-white w-full p-8 font-medium text-base md:text-4xl rounded cursor-pointer transition-all duration-500 shadow shadow-neutral-900 text-center flex gap-6 items-center ${optionsModel[i].color}`}
+                                >
+                                    {optionsModel[i].icon}{" "}
+                                    <span className="">{o}</span>
+                                </div>
+                            ))}
                         </div>
                     )}
                 </>
