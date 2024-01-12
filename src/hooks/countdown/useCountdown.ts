9dab9e2adf239d0ef1c8188fc229cb77b9592712
briansleonel <gonzalezbrianleonel76@gmail.com>
2024-01-12 14:16:41 -0300

    refractor: update hook to implement functions to start and reset timer

--- src/hooks/countdown/useCountdown.ts
+++ src/hooks/countdown/useCountdown.ts
@@ -1,5 +1,43 @@
 import { useEffect, useState, useRef } from "react";
 
+/**
+ * Hook personalizado que permite realizar una cuenta regresiva a partir de un valor inicial.
+ * Ofrece métodos para resetear la cuenta regresiva a sus valores iniciales, y para iniciar la cuenta regresiva
+ * @param initialTime valor inicial de la cuenta regresiva
+ * @param auto si debe iniciar automaticamente
+ * @returns countdos: cuenta regresiva - resetTimer: funcion para resetear los valores - startTimer: funcion para iniciar la cuenta regresiva
+ */
+export default function useCountdown(initialTime: number, auto = true) {
+    const [countdown, setCountdown] = useState(initialTime);
+    const [start, setStart] = useState(auto);
+
+    const timerID = useRef<NodeJS.Timeout>();
+
+    useEffect(() => {
+        if (start) {
+            if (countdown <= 0) {
+                return;
+            }
+
+            // Set up the timer
+            timerID.current = setInterval(() => {
+                setCountdown((prev) => prev - 1);
+            }, 1000);
+
+            // Clean up the timer
+            return () => clearInterval(timerID.current);
+        }
+    }, [countdown, start]);
+
+    function resetTimer(newTime?: number) {
+        setCountdown(newTime ?? initialTime);
+        setStart(false);
+    }
+
+    return { countdown, resetTimer, startTimer: setStart };
+}
+
+/*
 export default function useCountdown(initialTime: number, auto = true) {
     const [countdown, setCountdown] = useState(initialTime);
     const [start, setStart] = useState(auto);
@@ -21,3 +59,4 @@ export default function useCountdown(initialTime: number, auto = true) {
 
     return { countdown, setStart };
 }
+*/
