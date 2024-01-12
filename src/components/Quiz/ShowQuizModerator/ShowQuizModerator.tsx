146c6fa8259863a893922e9a97a01e7b3ec59219
briansleonel <gonzalezbrianleonel76@gmail.com>
2024-01-12 14:20:08 -0300

    feat: implements custoom hooks

--- src/components/Quiz/ShowQuizModerator/ShowQuizModerator.tsx
+++ src/components/Quiz/ShowQuizModerator/ShowQuizModerator.tsx
@@ -1,38 +1,21 @@
-import { useEffect, useState } from "react";
 import QuestionAndOptions from "../QuestionAndOptions/QuestionAndOptions";
-import { Player } from "../../../types/quiz/player";
-import { socket } from "../../../socket";
 
 import RankingModerator from "../RankingModerator/RankingModerator";
-import { useAppDispatch } from "../../../store/hooks.redux";
-import { quizNextQuestion } from "../../../store/features/quiz.slice";
+import useShowRankingModerator from "../../../hooks/quizModerator/useShowRankingModerator";
+import useShowQuizModerator from "../../../hooks/quizModerator/useShowQuizModerator";
 
 export default function ShowQuizModerator() {
-    const dispatch = useAppDispatch();
+    const {
+        countdownShowOptions,
+        countdownShowQuestion,
+        question,
+        showOptions,
+        showCorrect,
+        resetValues,
+    } = useShowQuizModerator();
 
-    const [showRanking, setShowRanking] = useState(false);
-    const [rankingPlayers, setRankingPlayers] = useState<Array<Player>>();
-
-    useEffect(() => {
-        function showRankingModerator(players: Array<Player>) {
-            setShowRanking(true);
-            setRankingPlayers(players);
-        }
-
-        function nextQuestion(hasNext: boolean, currentQuestion: number) {
-            dispatch(quizNextQuestion({ currentQuestion, hasNext }));
-            setShowRanking(false);
-            setRankingPlayers(undefined);
-            socket.emit("quiz:show-question");
-        }
-
-        socket.on("quiz:ranking-moderator", showRankingModerator);
-        socket.on("quiz:next-question", nextQuestion);
-
-        return () => {
-            socket.off("quiz:ranking-moderator", showRankingModerator);
-            socket.off("quiz:next-question", nextQuestion);
-        };
+    const { rankingPlayers, showRanking } = useShowRankingModerator({
+        resetValues,
     });
 
     return (
@@ -40,7 +23,13 @@ export default function ShowQuizModerator() {
             {showRanking && rankingPlayers ? (
                 <RankingModerator rankingPlayers={rankingPlayers} />
             ) : (
-                <QuestionAndOptions />
+                <QuestionAndOptions
+                    countdownShowOptions={countdownShowOptions}
+                    countdownShowQuestion={countdownShowQuestion}
+                    question={question}
+                    showCorrect={showCorrect}
+                    showOptions={showOptions}
+                />
             )}
         </>
     );
