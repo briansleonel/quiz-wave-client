a5d69167567e64bc1823d44544aa8ad666d5a658
briansleonel <gonzalezbrianleonel76@gmail.com>
2024-04-08 23:50:39 -0300

    feat: add socket auth token

--- src/socket.ts
+++ src/socket.ts
@@ -1,12 +1,16 @@
 import { Socket, io } from "socket.io-client";
 import { config } from "./config/config";
 import { ClientEvents, ServerEvents } from "./types/quiz/events.socket";
+import { loadStateTokenLocalStorage } from "./libs/token.localstorage";
 
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = config.URL_SOCKET;
 
 export const socket: Socket<ServerEvents, ClientEvents> = io(URL, {
     autoConnect: false,
+    auth: (cb) => {
+        cb({ token: loadStateTokenLocalStorage() });
+    },
 });
 
 //{autoConnect: false}
