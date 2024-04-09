3559bb1729d7422bddbffac044f66ef63f4e0714
briansleonel <gonzalezbrianleonel76@gmail.com>
2024-04-09 00:01:17 -0300

    styles: update styles of component

--- src/components/Quiz/ShowQuestion/ShowQuestion.tsx
+++ src/components/Quiz/ShowQuestion/ShowQuestion.tsx
@@ -7,7 +7,7 @@ export default function ShowQuestion({ question, showOptions }: Props) {
     return (
         <div
             className={`bg-white  text-center font-medium transition-all  ${
-                showOptions ? "p-4 py-5 text-3xl md:p-8 md:text-5xl" : "p-4 py-6 text-3xl md:p-12 md:text-6xl"
+                showOptions ? "p-4 py-5 text-3xl md:p-10 md:text-6xl" : "p-4 py-6 text-3xl md:p-12 md:text-7xl"
             }`}
         >
             {question}
