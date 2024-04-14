55f4f1abd4b4002672e3e34e0f7be9c326455841
briansleonel <gonzalezbrianleonel76@gmail.com>
2024-04-14 20:38:18 -0300

    feat: add object routes path

---
+++ src/libs/routes.ts
@@ -0,0 +1,11 @@
+const routes = {
+    index: "/",
+    dashboard: "/dashboard",
+    collections: "/dashboard/collection",
+    questions: "/dashboard/question",
+    categories: "/dashboard/category",
+    users: "/dashboard/user",
+    account: "/dashboard/account",
+};
+
+export default routes;
