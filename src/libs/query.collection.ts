fb7529bb7ab9caa3c66715ac58b3ec52dcf0076c
briansleonel <gonzalezbrianleonel76@gmail.com>
2024-04-14 20:40:09 -0300

    refractor ipdate query collections

--- src/libs/query.collection.ts
+++ src/libs/query.collection.ts
@@ -1,13 +1,13 @@
 interface Params {
-    recents: boolean;
-    user: string;
-    searchText: string;
+    recents?: boolean;
+    user?: string;
+    searchText?: string;
 }
 
 export function getQueryCollection({ recents, searchText, user }: Params) {
-    const recentQuery = `recents=${recents}`;
-    const userQuery = `user=${user === "all" ? "" : user}`;
-    const textQuery = `text=${searchText}`;
+    const recentQuery = `recents=${recents ?? ""}`;
+    const userQuery = `user=${user === "all" ? "" : user ?? ""}`;
+    const textQuery = `text=${searchText ?? ""}`;
 
     return `&${recentQuery}&${userQuery}&${textQuery}`;
 }
